4bf835dc7b4cf822c752b6947a073ca8dcc9c167
John Newcombe <johnfnewcombe@icloud.com>
2021-05-02 11:07:35 -0700

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by John Newcombe on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
